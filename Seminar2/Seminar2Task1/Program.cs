1c5e5af9649aaa54603b93ffc99c71de0433adc0
no4ta <elesin.p@yandex.ru>
2023-06-30 17:00:42 +0300

    Добавлено ДЗ ко 2-му семинару

---
+++ Seminar2/Seminar2Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трёхзначное число: ");
+int a = int.Parse(Console.ReadLine());
+string b = Convert.ToString(a);
+if (b.Length == 3)
+{
+    Console.WriteLine("Вторая цифра этого числа: " +b[1]);
+}
+else
+{
+    Console.WriteLine("Это не трёхзначное число");
+}
---
+++ Seminar2/Seminar2Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Введите число, в котором больше 2-х знаков: ");
+int a = int.Parse(Console.ReadLine());
+string b = Convert.ToString(a);
+if (b.Length > 2)
+{
+    Console.WriteLine("Третья цифра: " +b[2]);
+}
+else if (b.Length <= 2)
+{
+    Console.WriteLine("Вы ввели не подходящее под условие число!");
+}
