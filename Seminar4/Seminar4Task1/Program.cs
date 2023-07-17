c1f96d52d151d77451b975f581c50691a9d2f860
no4ta <elesin.p@yandex.ru>
2023-07-17 11:10:35 +0300

    изменение задачи 1

--- Seminar4/Seminar4Task1/Program.cs
+++ Seminar4/Seminar4Task1/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.WriteLine("введите число A");
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B
+Console.WriteLine("введите число A");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите число B");
 int b = Convert.ToInt32(Console.ReadLine());
